f21b979ef6d9960f815a2ce7196f13b37d0712db
Pruthvi Shetty <pruthvishetty5656@gmail.com>
2022-06-28 15:33:53 +0530

    Docs/docusaurus initial setup (#13)
    * Adds Docasaurus Setup
    
    * remove extra files
    
    * Adds button docs + hover config to docs
    
    * Adds Switch and Card Docs + Some minor fixes
    
    * Update package.json
    
    * fix booleans
    
    * fix netlify issues
    
    * change workspace
    
    * Update netlify.toml
    
    * Update netlify.toml
    
    * fixes around turbo
    
    * Update netlify.toml
    
    * Update netlify.toml
    
    * Update netlify.toml
    
    * Update docusaurus.config.js
    
    * Adds Vanilla Extract Plugin
    
    * Delete extra files

---
+++ docs/src/components/HomepageFeatures/index.tsx
@@ -0,0 +1,72 @@
+import React from "react";
+import clsx from "clsx";
+import styles from "./styles.module.css";
+
+type FeatureItem = {
+  title: string;
+  Svg: React.ComponentType<React.ComponentProps<"svg">>;
+  description: JSX.Element;
+};
+
+const FeatureList: FeatureItem[] = [
+  {
+    title: "Easy to Use",
+    Svg: require("@site/static/img/hover_logo_letters.svg").default,
+    description: (
+      <>
+        Hover focuses on easy to use battries included method, so use it as you
+        would use a native element, no need to learn fancy complex apis to do
+        simple things.
+      </>
+    ),
+  },
+  {
+    title: "Focus on What Matters",
+    Svg: require("@site/static/img/hover_logo_letters.svg").default,
+    description: (
+      <>
+        Easily extend the components and use it to conform to your brand
+        identity.
+      </>
+    ),
+  },
+  {
+    title: "Platform agnostic",
+    Svg: require("@site/static/img/hover_logo_letters.svg").default,
+    description: (
+      <>
+        Hover uses vanilla extract behind the hood, meainig most of the styles
+        are platform agnostic, if it can run css and js then hover works!
+      </>
+    ),
+  },
+];
+
+function Feature({ title, Svg, description }: FeatureItem) {
+  return (
+    <div className={clsx("col col--4")}>
+      <div className="text--center">
+        <Svg className={styles.featureSvg} role="img" />
+      </div>
+
+      <div className="text--center padding-horiz--md">
+        <h3>{title}</h3>
+        <p>{description}</p>
+      </div>
+    </div>
+  );
+}
+
+export default function HomepageFeatures(): JSX.Element {
+  return (
+    <section className={styles.features}>
+      <div className="container">
+        <div className="row">
+          {FeatureList.map((props, idx) => (
+            <Feature key={idx} {...props} />
+          ))}
+        </div>
+      </div>
+    </section>
+  );
+}
--- lib/src/index.ts
+++ lib/src/index.ts
@@ -1,3 +1,4 @@
 export * from "./components/Button";
 export * from "./components/Card";
 export * from "./components/reset";
+export * from "./components/Switch";
