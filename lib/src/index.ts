997e694818a98b7718df929f81ec433fda89955b
Raalzz <30571073+Raalzz@users.noreply.github.com>
2022-07-13 14:55:24 +0530

    HOV-3 - Adds Badge Component [V1]  (#16)
    * feat: created badge component
    
    * refactor: refactored variants and types, included ref
    
    * feat: Added Docs
    
    * refactor: Updated ref and role attribute

--- lib/src/index.ts
+++ lib/src/index.ts
@@ -2,5 +2,6 @@ export * from "./components/Button";
 export * from "./components/Card";
 export * from "./components/reset";
 export * from "./components/Switch";
+export * from "./components/Badge";
 export * from "./components/List";
 export * from "./components/ListItem";
