72d7922af698288d0bc507c11f70125506b7d193
hafsahayath <44601750+hafsahayath@users.noreply.github.com>
2022-07-18 11:15:00 +0530

    refactor: extend switch component

--- lib/src/components/Switch/Switch.tsx
+++ lib/src/components/Switch/Switch.tsx
@@ -2,14 +2,18 @@ import { slider, switchInputStyle, switchLayout } from "./switch.css";
 import { ISwitchProps } from "./switch.types";
 import "./switch-global-styles.css";
 
+type SwitchProps = ISwitchProps & JSX.IntrinsicElements["input"];
+
 export const Switch = ({
   status,
   onChange,
   isDisabled = false,
-}: ISwitchProps) => {
+  ...nativeProps
+}: SwitchProps) => {
   return (
     <label className={switchLayout}>
       <input
+        {...nativeProps}
         className={switchInputStyle}
         type="checkbox"
         checked={status}
