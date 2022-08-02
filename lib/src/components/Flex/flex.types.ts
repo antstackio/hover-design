2f985543f4dc8f69bb0dfdcca557b2f5e4bf7f6b
Shadow-Flash <varun.gupta@antstack.io>
2022-08-03 01:41:07 +0530

    [UPDATE] flex types

--- lib/src/components/Flex/flex.types.ts
+++ lib/src/components/Flex/flex.types.ts
@@ -1,11 +1,12 @@
 export interface FlexProps extends React.DetailedHTMLProps<React.HTMLAttributes<HTMLDivElement>, HTMLDivElement>
 {
-    alignContent?: 'start' | 'center' | 'end' | 'space-between' | 'space-around' | 'space-evenly' | 'stretch' | 'flex-start' | 'flex-end' | 'normal' | 'baseline' | 'first baseline' | 'last baseline' | 'safe center' | 'unsafe center' | 'inherit' | 'initial' | 'revert' | 'revert-layer' | 'unset',
-    alignItems?: 'normal' | 'stretch' | 'center' | 'start' | 'end' | 'flex-start' | 'flex-end' | 'baseline' | 'first baseline' | 'last baseline' | 'safe center' | 'unsafe center' | 'inherit' | 'initial' | 'revert' | 'revert-layer' | 'unset',
-    flexDirection?: 'row' | 'row-reverse' | 'column' | 'column-reverse',
-    flexWrap?: 'nowrap' | 'wrap' | 'wrap-reverse',
-    justifyContent?: 'normal' | 'left' | 'right' | 'center' | 'start' | 'end' | 'flex-start' | 'flex-end' | 'space-between' | 'space-around' | 'space-evenly' | 'stretch' | 'safe center' | 'unsafe center' | 'inherit' | 'initial' | 'revert' | 'revert-layer' | 'unset',
-    flexShrink?: number,
-    flexGrow?: number,
-    flexBasis?: string | 'auto' | 'max-content' | 'min-content' | 'fit-content' | 'content' | 'inherit' | 'initial' | 'revert' | 'revert-layer' | 'unset',
+    alignContent?: React.CSSProperties['alignContent'],
+    alignItems?: React.CSSProperties['alignItems'],
+    flexDirection?: React.CSSProperties['flexDirection'],
+    flexWrap?: React.CSSProperties['flexWrap'],
+    justifyContent?: React.CSSProperties['justifyContent'],
+    flexShrink?: React.CSSProperties['flexShrink'],
+    flexGrow?: React.CSSProperties['flexGrow'],
+    flexBasis?: React.CSSProperties['flexBasis'],
+    gap:React.CSSProperties['gap'],
 }
