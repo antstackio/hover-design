810994c5654839f6f32248684e84598a45040cb4
Shadow-Flash <varun.gupta@antstack.io>
2022-08-03 13:51:26 +0530

    [ADDED] flex-gap property

--- lib/src/components/Flex/Flex.tsx
+++ lib/src/components/Flex/Flex.tsx
@@ -1,8 +1,8 @@
-import React from "react";
+import React, { ForwardRefRenderFunction } from "react";
 import { FlexProps } from "./flex.types";
 import { assignInlineVars } from "@vanilla-extract/dynamic";
 
-export const Flex: React.FC<FlexProps> = ({
+export const FlexComponent:ForwardRefRenderFunction<HTMLUListElement, FlexProps> = ({
     alignContent = 'normal',
     alignItems = 'flex-start',
     flexDirection = 'row',
@@ -11,10 +11,11 @@ export const Flex: React.FC<FlexProps> = ({
     flexShrink = 1,
     flexGrow = 1,
     flexBasis = 'auto',
+    gap = 'normal',
     children,
     style,
     ...nativeProps
-}) => {
+}, ref) => {
   const flexStyles = assignInlineVars({
     display: 'flex',
     alignContent,
@@ -24,15 +25,19 @@ export const Flex: React.FC<FlexProps> = ({
     justifyContent,
     flexShrink: flexShrink.toString(),
     flexGrow: flexGrow.toString(),
-    flexBasis
+    flexBasis: flexBasis.toString(),
+    gap: gap.toString()
   });
   /**
    * If user passes style object it will merge with current style,
-   * TODO: Not able to show the autocomplete on Flex Component for style,
+   * TODO: Not able to show the autocomplete on Flex Component for style, and I am not able to make use of ref
    */
   Object.assign(flexStyles, style); 
   
   return (
     <div style={flexStyles} {...nativeProps} > {children} </div>
     )
-}
+};
+
+const FlexWithRef = React.forwardRef(FlexComponent);
+export { FlexWithRef as Flex };
