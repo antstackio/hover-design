5d46ac296dce1429ad9de478871a8672192d8245
Pruthvi Shetty <pruthvishetty5656@gmail.com>
2022-08-14 00:12:19 +0530

    Squashed commit of the following:
    commit fb57a63f4b52912738cf8dcb39adbffad6802419
    Author: Saurabh Sutar <81956858+saurabhsutar192@users.noreply.github.com>
    Date:   Fri Aug 12 14:13:36 2022 +0530
    
        HOV-16 | Divider component (#53)
    
        * divider component v1 created
    
        * typos fixed
    
        * mdx changes
    
        * divider default color changed
    
        * mdx changes
    
        * comments resolved
    
    commit 2f5f8428b278e98ef5409a71aae56580cd3a715f
    Author: Pruthvi Shetty <pruthvishetty5656@gmail.com>
    Date:   Fri Aug 12 14:10:43 2022 +0530
    
        Adds git playbook + adds steps for forking in contribution (#57)
    
    commit fcc1d6aa39ab2b8e851a70f0e3ce091c24c1c695
    Author: Pruthvi Shetty <pruthvishetty5656@gmail.com>
    Date:   Fri Aug 12 14:09:52 2022 +0530
    
        move react to peerDep (#55)

---
+++ lib/src/components/Divider/divider.css.ts
@@ -0,0 +1,156 @@
+import { createTheme, style } from "@vanilla-extract/css";
+import { recipe } from "@vanilla-extract/recipes";
+import { DividerTheme } from "./divider.types";
+import { calc } from "@vanilla-extract/css-utils";
+
+export const [dividerThemeClass, dividerThemeVar]: DividerTheme = createTheme({
+  dividerColor: "#000",
+  labelColor: "#000",
+  labelBackground: "#fff",
+  dividerSize: "1px",
+});
+
+export const dividerContainerHorizontal = style({
+  position: "relative",
+  width: "100%",
+  height: "fit-content",
+});
+
+export const dividerContainerVertical = style({
+  position: "relative",
+  height: "100%",
+  width: "fit-content",
+});
+
+const dividerBaseStyles = style({
+  position: "relative",
+  background: "none",
+  backgroundPosition: "center",
+});
+
+export const dividerHorizontal = recipe({
+  base: [
+    dividerBaseStyles,
+    {
+      backgroundRepeat: "repeat-x",
+      height: `${dividerThemeVar.dividerSize}`,
+      width: "100%",
+    },
+  ],
+
+  variants: {
+    type: {
+      dashed: {
+        backgroundImage: `linear-gradient(
+          to right,
+          ${dividerThemeVar.dividerColor} 60%,
+          rgba(255, 255, 255, 0) 0%
+        )`,
+        backgroundSize: `${calc.multiply(dividerThemeVar.dividerSize, 5)} ${
+          dividerThemeVar.dividerSize
+        }`,
+      },
+      dotted: {
+        backgroundImage: `linear-gradient(
+          to right,
+          ${dividerThemeVar.dividerColor} 40%,
+          rgba(255, 255, 255, 0) 0%
+        )`,
+        backgroundSize: `${calc.multiply(dividerThemeVar.dividerSize, 3)} ${
+          dividerThemeVar.dividerSize
+        }`,
+      },
+      solid: {
+        background: dividerThemeVar.dividerColor,
+      },
+    },
+  },
+});
+
+export const dividerVertical = recipe({
+  base: [
+    dividerBaseStyles,
+    {
+      backgroundRepeat: "repeat-y",
+      height: "100%",
+      width: `${dividerThemeVar.dividerSize}`,
+    },
+  ],
+
+  variants: {
+    type: {
+      dashed: {
+        backgroundImage: `linear-gradient(
+          to bottom,
+          ${dividerThemeVar.dividerColor} 60%,
+          rgba(255, 255, 255, 0) 0%
+        )`,
+        backgroundSize: `${dividerThemeVar.dividerSize} ${calc.multiply(
+          dividerThemeVar.dividerSize,
+          5
+        )}`,
+      },
+      dotted: {
+        backgroundImage: `linear-gradient(
+          to bottom,
+          ${dividerThemeVar.dividerColor} 40%,
+          rgba(255, 255, 255, 0) 0%
+        )`,
+        backgroundSize: `${dividerThemeVar.dividerSize} ${calc.multiply(
+          dividerThemeVar.dividerSize,
+          3
+        )}`,
+      },
+      solid: {
+        background: dividerThemeVar.dividerColor,
+      },
+    },
+  },
+});
+
+const labelBaseStyles = style({
+  background: dividerThemeVar.labelBackground,
+  color: dividerThemeVar.labelColor,
+  padding: "4px 8px",
+  position: "absolute",
+});
+
+export const labelHorizontal = recipe({
+  base: [labelBaseStyles, { top: "50%", transform: "translateY(-50%)" }],
+  variants: {
+    labelPosition: {
+      start: {
+        left: 0,
+        right: "auto",
+      },
+      center: {
+        left: "50%",
+        transform: "translate(-50%,-50%)",
+      },
+      end: {
+        left: "auto",
+        right: 0,
+      },
+    },
+  },
+});
+
+export const labelVertical = recipe({
+  base: [labelBaseStyles, { left: "50%", transform: "translateX(-50%)" }],
+  variants: {
+    labelPosition: {
+      start: {
+        top: 0,
+        bottom: "auto",
+      },
+      center: {
+        top: "50%",
+        transform: "translate(-50%,-50%)",
+      },
+      end: {
+        top: "auto",
+        bottom: 0,
+      },
+    },
+  },
+});
