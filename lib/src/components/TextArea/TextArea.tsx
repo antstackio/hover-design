2a6855dd447d442be3855acff493e9b1141f34d5
Pruthvi Shetty <pruthvishetty5656@gmail.com>
2022-08-18 07:34:22 +0530

    minor change

--- lib/src/components/TextArea/TextArea.tsx
+++ lib/src/components/TextArea/TextArea.tsx
@@ -1,37 +1,49 @@
 import { assignInlineVars } from "@vanilla-extract/dynamic";
 import React, { ForwardRefRenderFunction } from "react";
-import { textAreaStyle, textAreaResize, textAreaColorClass, textAreaColorVars } from "./text-area.css";
+import {
+  textAreaStyle,
+  textAreaResize,
+  textAreaColorClass,
+  textAreaColorVars,
+} from "./text-area.css";
 import { TextAreaProps } from "./text-area.types";
 
-const TextAreaComponent: ForwardRefRenderFunction<HTMLTextAreaElement, TextAreaProps> = ({ 
+const TextAreaComponent: ForwardRefRenderFunction<
+  HTMLTextAreaElement,
+  TextAreaProps
+> = (
+  {
     className,
     children,
     style,
     fixedSize = false,
-    statusBorder = '#082D59',
+    statusBorder = "#082D59",
     ...nativeTextAreaProps
- }, ref) => {
-
+  },
+  ref
+) => {
   let textAreaBorder = assignInlineVars({
-    [textAreaColorVars.textAreaBorderColor.statusBorderColor]: statusBorder
+    [textAreaColorVars.textAreaBorderColor.statusBorderColor]: statusBorder,
   });
 
   /**
-   * It will overwrite the default styling with the user provided style 
+   * It will overwrite the default styling with the user provided style
    */
   Object.assign(textAreaBorder, style);
 
   return (
-    <textarea 
-      className={`${textAreaStyle} ${className} ${fixedSize ? textAreaResize : null} ${textAreaColorClass}`} 
+    <textarea
+      className={`${textAreaStyle} ${className} ${
+        fixedSize ? textAreaResize : null
+      } ${textAreaColorClass}`}
       style={textAreaBorder}
-      ref={ref} 
+      ref={ref}
       {...nativeTextAreaProps}
-      >
-        {children}
+    >
+      {children}
     </textarea>
   );
 };
 
 const TextAreaWithRef = React.forwardRef(TextAreaComponent);
-export { TextAreaWithRef as Textarea };
+export { TextAreaWithRef as TextArea };
