41d23a8b1d8b2e46b75ab3bc51a786c59b0a40c7
hafsahayath <44601750+hafsahayath@users.noreply.github.com>
2022-09-02 13:21:36 +0530

    move global styles and update docs

--- docs/src/components/examples/resetTableStyles.css.ts
+++ docs/src/components/examples/resetTableStyles.css.ts
@@ -1,22 +1,3 @@
-import { globalStyle, style } from "@vanilla-extract/css";
+import { style } from "@vanilla-extract/css";
 
 export const resetTableStyles = style({});
-
-// reset table styles
-globalStyle(
-  `${resetTableStyles} td, ${resetTableStyles} th,  ${resetTableStyles} tr`,
-  {
-    border: "none",
-  }
-);
-
-globalStyle(
-  `${resetTableStyles} tr:nth-child(even), ${resetTableStyles} thead`,
-  {
-    backgroundColor: "transparent",
-  }
-);
-
-globalStyle(`${resetTableStyles} table`, {
-  marginBottom: 0,
-});
