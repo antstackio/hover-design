a198190b1270a0f43e3b95bfb10fb016a18129de
Raalzz <30571073+Raalzz@users.noreply.github.com>
2022-09-02 18:10:43 +0530

    Adds Radio and RadioGroup [v1] (#85)
    * feat: Adds radio Button and Radio Group
    
    * refactor: Refactored radio themes
    
    * feat: Updated Docs
    
    * refactor: Resolved git comments

---
+++ lib/src/components/Radio/radio.global.styles.css.ts
@@ -0,0 +1,28 @@
+import { globalStyle } from "@vanilla-extract/css";
+import { radioThemeVars, radioWrapperClass } from "./radio.styles.css";
+
+globalStyle(`${radioWrapperClass} > svg`, {
+  display: "inline-block",
+  verticalAlign: "middle"
+});
+
+globalStyle(`${radioWrapperClass} > input[type="radio"]`, {
+  opacity: "0.0001",
+  position: "absolute",
+  width: "100%",
+  height: "100%",
+  top: "-2px",
+  left: "-4px",
+  cursor: "default"
+});
+
+globalStyle(`${radioWrapperClass} >  [data-checked="true"]`, {
+  color: radioThemeVars.selectedStyles.color,
+  borderColor: radioThemeVars.selectedStyles.borderColor,
+  backgroundColor: radioThemeVars.selectedStyles.backgroundColor
+});
+
+globalStyle(`${radioWrapperClass} >  [data-disabled="true"]`, {
+  borderColor: radioThemeVars.disabledStyles.borderColor,
+  backgroundColor: radioThemeVars.disabledStyles.backgroundColor
+});
