0102db60adf7ccc3d6ae1443755535cfb290eb29
Shadow-Flash <varun.gupta@antstack.io>
2022-09-09 15:35:33 +0530

    [UPDATE] type is compulsory

--- lib/src/components/DateAndTime/date-time-picker.types.ts
+++ lib/src/components/DateAndTime/date-time-picker.types.ts
@@ -4,5 +4,5 @@ export interface DateTimePickerProps extends React.DetailedHTMLProps<
 React.InputHTMLAttributes<HTMLInputElement>,
 HTMLInputElement
 > {
-    type?: "date" | "datetime-local" | "month" | "week" | "time";
+    type: "date" | "datetime-local" | "month" | "week" | "time";
 }
