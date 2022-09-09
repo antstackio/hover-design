2fe52fbc014d85fb073388737c816bfd79167d6f
saurabhsutar192 <saurabhsutar192@gmail.com>
2022-09-09 16:18:40 +0530

    multiSelect with Keyboard controls complete

--- lib/src/components/Select/Select.tsx
+++ lib/src/components/Select/Select.tsx
@@ -14,6 +14,7 @@ import { useClickOutside } from "src/hooks/useClickOutside";
 import { Flex } from "../Flex";
 import { ArrowDown } from "../_internal/Icons/ArrowDown";
 import { Clear } from "../_internal/Icons/Clear";
+import { Pill } from "./Pill/Pill";
 import {
   selectContainerStyles,
   selectErrorMsg,
@@ -37,7 +38,6 @@ const SelectComponent: ForwardRefRenderFunction<
     placeholder,
     options,
     value,
-    height = "40px",
     width = "100%",
     borderRadius = "0",
     color = "#2F80ED",
@@ -55,11 +55,16 @@ const SelectComponent: ForwardRefRenderFunction<
   },
   ref
 ) => {
-  const [selectValue, setSelectValue] = useState(value);
+  const [selectValue, setSelectValue] = useState<
+    string | number | (string | number)[] | undefined
+  >(value);
   const [isDropped, setIsDropped] = useState(false);
   const [internalOptions, setInternalOptions] = useState(options);
   const [searchText, setSearchText] = useState("");
   const selectRef = useRef() as MutableRefObject<HTMLDivElement>;
+  const inputRef = useRef() as MutableRefObject<HTMLInputElement>;
+  const optionsListRef = useRef() as MutableRefObject<HTMLDivElement>;
+  const [cursor, setCursor] = useState(0);
 
   useEffect(() => {
     if (value !== undefined) {
@@ -68,49 +73,102 @@ const SelectComponent: ForwardRefRenderFunction<
   }, [value]);
 
   useEffect(() => {
-    setSearchText(
-      options.find((option) => option.value === selectValue)?.label || ""
+    if (isMulti) {
+      typeof selectValue === "object" &&
+        setInternalOptions(
+          options.filter((option) => !selectValue.includes(option.value))
+        );
+    } else {
+      setInternalOptions(options);
+      setSearchText(getLabel());
+    }
+  }, [selectValue, isMulti]);
+
+  useEffect(() => {
+    focusElement(cursor);
+  }, [cursor]);
+
+  useEffect(() => {
+    if (optionsListRef.current && inputRef.current) {
+      optionsListRef.current.style.top = `${
+        inputRef.current.offsetHeight - 5
+      }px`;
+    }
+  }, [inputRef, optionsListRef, isDropped, selectValue]);
+
+  const getLabel = (extValue?: string | number) => {
+    return (
+      options.find((option) => option.value === (extValue || selectValue))
+        ?.label || ""
     );
-    setInternalOptions(options);
-  }, [selectValue]);
+  };
+
+  const focusElement = (pointer: number) => {
+    if (optionsListRef.current) {
+      const optionsList = [
+        ...optionsListRef.current.childNodes,
+      ] as HTMLElement[];
+      optionsList[pointer].focus();
+    }
+  };
 
   const internalClickHandler = (
     option: OptionsType,
-    event: MouseEvent<HTMLSpanElement> | KeyboardEvent<HTMLSpanElement>
+    event:
+      | MouseEvent<HTMLDivElement>
+      | KeyboardEvent<HTMLDivElement>
+      | MouseEvent<SVGSVGElement>
   ) => {
-    if (value !== undefined) {
-      isClearable && option.value === selectValue
-        ? onChange("", event)
-        : onChange(option.value, event);
+    if (isMulti) {
+      setSearchText("");
+      const multiValue =
+        typeof selectValue === "object" ? [...selectValue] : [];
+      multiValue.push(option.value);
+      if (value !== undefined) {
+        onChange(multiValue, event);
+      } else {
+        setSelectValue(multiValue);
+      }
     } else {
-      isClearable && option.value === selectValue
-        ? setSelectValue("")
-        : setSelectValue(option.value);
+      if (value !== undefined) {
+        isClearable && option.value === selectValue
+          ? onChange("", event)
+          : onChange(option.value, event);
+      } else {
+        isClearable && option.value === selectValue
+          ? setSelectValue("")
+          : setSelectValue(option.value);
+      }
+      setIsDropped(false);
+      setInternalOptions(options);
+      onDropDownClose();
     }
-    setInternalOptions(options);
-    setIsDropped(false);
-    onDropDownClose();
   };
 
   const internalChangeHandler = (event: ChangeEvent<HTMLInputElement>) => {
+    event.stopPropagation();
     const text = event.target.value;
+    const mainOptions =
+      isMulti && typeof selectValue === "object"
+        ? options.filter((option) => !selectValue.includes(option.value))
+        : options;
     setIsDropped(true);
     setSearchText(text);
-    const filteredOptions = options.filter((option) =>
-      option.label.toLowerCase().includes(text.toLowerCase())
-    );
+    const filteredOptions = mainOptions.filter((option) => {
+      return option.label.toLowerCase().includes(text.toLowerCase());
+    });
     text === ""
-      ? setInternalOptions(options)
+      ? setInternalOptions(mainOptions)
       : setInternalOptions(filteredOptions);
   };
 
-  const handleIconClick = (event: MouseEvent<HTMLSpanElement>) => {
+  const handleIconClick = (event: MouseEvent<HTMLDivElement>) => {
     event.stopPropagation();
     if (isClearable) {
       if (value !== undefined) {
-        onChange("", event);
+        isMulti ? onChange([], event) : onChange("", event);
       } else {
-        setSelectValue("");
+        isMulti ? setSelectValue([]) : setSelectValue("");
       }
     } else {
       setIsDropped(!isDropped);
@@ -123,6 +181,113 @@ const SelectComponent: ForwardRefRenderFunction<
     isDropped ? onDropDownClose() : onDropDownOpen();
   };
 
+  const clearPill = (
+    clearValue: string | number,
+    event: MouseEvent<SVGSVGElement>
+  ) => {
+    let tempArr = typeof selectValue === "object" ? [...selectValue] : [];
+    tempArr = tempArr.filter((arr) => arr !== clearValue);
+    if (value !== undefined) {
+      onChange(tempArr, event);
+    } else {
+      setSelectValue(tempArr);
+    }
+    setIsDropped(true);
+  };
+
+  const checkIfAllValuesSelected = () => {
+    return (
+      typeof selectValue === "object" &&
+      selectValue.length === options.length &&
+      options.every((el) => selectValue.includes(el.value))
+    );
+  };
+
+  const handleInputKeyChange = (event: KeyboardEvent<HTMLDivElement>) => {
+    if (event.key === "ArrowDown") {
+      setIsDropped(true);
+      setTimeout(() => focusFirstOption());
+    }
+    if (event.key === "Enter" || event.code === "Space") {
+      event.preventDefault();
+      changeDrop();
+    }
+  };
+
+  const focusNextOption = () => {
+    let skipStep = 1;
+    while (
+      cursor + skipStep < internalOptions.length &&
+      internalOptions[cursor + skipStep].disabled
+    ) {
+      skipStep++;
+    }
+    cursor + skipStep < internalOptions.length && setCursor(cursor + skipStep);
+  };
+
+  const focusPrevOption = () => {
+    let skipStep = 1;
+    while (
+      cursor - skipStep >= 0 &&
+      internalOptions[cursor - skipStep].disabled
+    ) {
+      skipStep++;
+    }
+    cursor - skipStep >= 0 && setCursor(cursor - skipStep);
+  };
+
+  const focusFirstOption = () => {
+    let skipStep = 0;
+    while (
+      skipStep < internalOptions.length &&
+      internalOptions[skipStep].disabled
+    ) {
+      skipStep++;
+    }
+    if (skipStep < internalOptions.length) {
+      setCursor(skipStep);
+      focusElement(skipStep);
+    }
+  };
+
+  const focusLastOption = () => {
+    let skipStep = 1;
+    while (
+      internalOptions.length - skipStep >= 0 &&
+      internalOptions[internalOptions.length - skipStep].disabled
+    ) {
+      skipStep++;
+    }
+    internalOptions.length - skipStep >= 0 &&
+      setCursor(internalOptions.length - skipStep);
+  };
+
+  const handleOptionKeyChange = (
+    event: KeyboardEvent<HTMLDivElement>,
+    option: OptionsType
+  ) => {
+    switch (event.key) {
+      case "ArrowDown":
+        focusNextOption();
+        break;
+      case "ArrowUp":
+        focusPrevOption();
+        break;
+      case "Home":
+        focusFirstOption();
+        break;
+      case "End":
+        focusLastOption();
+        break;
+      default:
+        break;
+    }
+
+    event.key !== "Tab" && event.preventDefault();
+    (event.key === "Enter" || event.code === "Space") &&
+      !option.disabled &&
+      internalClickHandler(option, event);
+  };
   const selectIconClass = selectIconRecipe({
     isDropped,
   });
@@ -137,6 +302,7 @@ const SelectComponent: ForwardRefRenderFunction<
   });
   const inputStyles = inputRecipe({
     error: error ? true : false,
+    isMulti,
   });
 
   useClickOutside(
@@ -163,27 +329,42 @@ const SelectComponent: ForwardRefRenderFunction<
       style={assignInlineVars({
         [selectVars.borderRadius]: borderRadius,
         [selectVars.color]: color,
-        [selectVars.height]: height,
         [selectVars.width]: width,
         [selectVars.maxDropDownHeight]: maxDropDownHeight,
       })}
     >
       <Flex
-        role="selectbox"
+        role="combobox"
+        ref={inputRef}
         aria-expanded={isDropped}
         tabIndex={0}
         alignItems="center"
         justifyContent={"space-between"}
         className={`${selectInputStyles}`}
         onClick={changeDrop}
-        onKeyDown={(event) => {
-          if (event.key === "Enter") {
-            changeDrop();
-          }
-        }}
+        onKeyDown={handleInputKeyChange}
       >
-        <div className={inputTextContainer}>
-          {isMulti && <div></div>} {/*pill container */}
+        <Flex
+          className={inputTextContainer}
+          gap="4px"
+          flexWrap="wrap"
+          alignItems="center"
+        >
+          {isMulti &&
+            (typeof selectValue === "object" && selectValue.length !== 0
+              ? selectValue?.map((arr) => {
+                  return (
+                    <Pill
+                      value={getLabel(arr)}
+                      clearValue={(event) => clearPill(arr, event)}
+                    />
+                  );
+                })
+              : !isSearchable && (
+                  <div className={selectPlaceholder}>
+                    {placeholder || "Pick anything!"}
+                  </div>
+                ))}
           {isSearchable && (
             <input
               disabled={isDisabled}
@@ -191,32 +372,33 @@ const SelectComponent: ForwardRefRenderFunction<
               value={searchText}
               placeholder={placeholder || "Search here"}
               onChange={internalChangeHandler}
+              onKeyDown={(event) => event.stopPropagation()}
             />
           )}
-          {!isMulti && !isSearchable && (
-            <div>
-              {options.find((option) => option.value === selectValue)
-                ?.label || (
-                <div className={selectPlaceholder}>
-                  {placeholder || "Pick one"}
-                </div>
-              )}
-            </div>
-          )}
-        </div>
+          {!isMulti &&
+            !isSearchable &&
+            (getLabel() || (
+              <div className={selectPlaceholder}>
+                {placeholder || "Pick one"}
+              </div>
+            ))}
+        </Flex>
 
         <Flex
           alignItems="center"
           className={selectIconClass}
           onClick={handleIconClick}
         >
-          {DropIcon ? !isClearable && DropIcon : !isClearable && <ArrowDown />}
-          {isClearable && <Clear />}
+          {DropIcon
+            ? !isClearable && DropIcon
+            : !isClearable && <ArrowDown width={18} height={18} />}
+          {isClearable && <Clear width={18} height={18} />}
         </Flex>
       </Flex>
 
       {isDropped && (
         <Flex
+          ref={optionsListRef}
           flexDirection="column"
           className={`${selectListContainerStyle}`}
           role={"listbox"}
@@ -226,32 +408,30 @@ const SelectComponent: ForwardRefRenderFunction<
             internalOptions.map((option, ind) => {
               const selectListClass = selectListRecipe({
                 disabled: option.disabled,
-                active: option.value === selectValue,
+                active: !isMulti && option.value === selectValue,
               });
               return (
-                <span
+                <div
                   key={ind}
                   ref={option.ref}
                   role="option"
-                  tabIndex={option.disabled ? 1 : 0}
+                  tabIndex={-1}
                   aria-selected={option.value === selectValue}
                   className={selectListClass}
                   onClick={(event) =>
                     !option.disabled && internalClickHandler(option, event)
                   }
-                  onKeyDown={(event) =>
-                    event.key === "Enter" &&
-                    !option.disabled &&
-                    internalClickHandler(option, event)
-                  }
+                  onKeyDown={(event) => handleOptionKeyChange(event, option)}
                 >
-                  {option.label}
-                </span>
+                  <span>{option.label}</span>
+                </div>
               );
             })
           ) : (
             <div className={noDataFoundStyles}>
-              {nothingFoundLabel || "Nothing Found!"}
+              {checkIfAllValuesSelected()
+                ? "No more Data!"
+                : nothingFoundLabel || "Nothing Found!"}
             </div>
           )}
         </Flex>
