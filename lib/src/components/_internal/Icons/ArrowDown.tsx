b933f75117c58efe6a356dd6d038199d7725a942
Pruthvi Shetty <pruthvishetty5656@gmail.com>
2022-09-20 11:58:34 +0530

    HOV-45 | Fixes Icon component to have lesser base values

--- lib/src/components/_internal/Icons/ArrowDown.tsx
+++ lib/src/components/_internal/Icons/ArrowDown.tsx
@@ -4,7 +4,17 @@ import { IIconProps } from "src/components/Icon/icon.type";
 
 export const ArrowDown: FC<IIconProps> = (props) => {
   return (
-    <Icon {...props}>
+    <Icon
+      width="24"
+      height="24"
+      viewBox="0 0 24 24"
+      strokeWidth="2"
+      stroke="currentColor"
+      fill="none"
+      strokeLinecap="round"
+      strokeLinejoin="round"
+      {...props}
+    >
       <path stroke="none" d="M0 0h24v24H0z" fill="none"></path>
       <line x1={12} y1={5} x2={12} y2={19}></line>
       <line x1={18} y1={13} x2={12} y2={19}></line>
