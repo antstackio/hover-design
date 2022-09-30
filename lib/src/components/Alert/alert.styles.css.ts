6fc209d69fd36c2249a319b3b9a67993cbc99c46
Saurabh Sutar <81956858+saurabhsutar192@users.noreply.github.com>
2022-09-30 12:20:44 +0530

    HOV-29 | Adds Alert component  (#124)
    * alert-component completed
    
    * button stories fix
    
    * nativeProps added
    
    * minor type changes
    
    * icons changed
    
    * alert title colorified
    
    * removed code smells
    
    Co-authored-by: Pruthvi Shetty <pruthvishetty5656@gmail.com>

---
+++ lib/src/components/Alert/alert.styles.css.ts
@@ -0,0 +1,88 @@
+import { createTheme, style } from "@vanilla-extract/css";
+import { recipe } from "@vanilla-extract/recipes";
+
+export const [alertInfoTheme, alertVars]: any = createTheme({
+  color: "#2F80ED",
+  backgroundColor: "#DEEBFF",
+});
+
+export const alertSuccessTheme: any = createTheme(alertVars, {
+  color: "#23A047",
+  backgroundColor: "#EEFFF3",
+});
+
+export const alertWarningTheme: any = createTheme(alertVars, {
+  color: "#FCA004",
+  backgroundColor: "#FFFBED",
+});
+export const alertDangerTheme: any = createTheme(alertVars, {
+  color: "#F44336",
+  backgroundColor: "#FFF6F8",
+});
+
+export const alertRecipe = recipe({
+  base: {
+    position: "relative",
+    width: "100%",
+    padding: "12px 16px",
+  },
+  variants: {
+    variant: {
+      light: {
+        border: `1px solid ${alertVars.color}`,
+        backgroundColor: alertVars.backgroundColor,
+      },
+      outline: {
+        border: `1px solid ${alertVars.color}`,
+        backgroundColor: "transparent",
+      },
+      filled: {
+        color: "white",
+        backgroundColor: alertVars.color,
+      },
+    },
+  },
+});
+
+export const alertTitleRecipe = recipe({
+  base: {
+    fontWeight: "700",
+    marginBottom: "7px",
+    color: alertVars.color,
+  },
+  variants: {
+    isFilled: {
+      true: { color: "white" },
+    },
+  },
+});
+
+export const alertIconContainerStyles = style({
+  marginRight: "10px",
+});
+
+export const alertIconRecipe = recipe({
+  base: {
+    color: alertVars.color,
+  },
+  variants: {
+    isFilled: {
+      true: {
+        color: "white",
+      },
+    },
+  },
+});
+
+export const alertCloseIconStyles = style({
+  cursor: "pointer",
+  position: "absolute",
+  right: "10px",
+  top: "10px",
+  border: "none",
+  display: "flex",
+  padding: 0,
+  justifyContent: "center",
+  alignItems: "center",
+  background: "transparent",
+});
---
+++ lib/src/components/_internal/Icons/CheckCircle.tsx
@@ -0,0 +1,25 @@
+import React from "react";
+import { Icon } from "../../../components/Icon";
+import { IIconProps } from "../../../components/Icon/icon.type";
+
+const CheckCircle: React.FC<IIconProps> = (props) => {
+  return (
+    <Icon
+      width="24"
+      height="24"
+      viewBox="0 0 24 24"
+      strokeWidth="2"
+      stroke="currentColor"
+      fill="none"
+      strokeLinecap="round"
+      strokeLinejoin="round"
+      {...props}
+    >
+      <path stroke="none" d="M0 0h24v24H0z" fill="none"></path>
+      <circle cx="12" cy="12" r="9"></circle>
+      <path d="M9 12l2 2l4 -4"></path>
+    </Icon>
+  );
+};
+
+export default CheckCircle;
