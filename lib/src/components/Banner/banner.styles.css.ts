526d198ad9ddfa232cd436c3ead9dae8350b58bc
saurabhsutar192 <saurabhsutar192@gmail.com>
2022-10-11 20:07:10 +0530

    removed code smells

--- lib/src/components/Banner/banner.styles.css.ts
+++ lib/src/components/Banner/banner.styles.css.ts
@@ -1,4 +1,4 @@
-import { createVar, style } from "@vanilla-extract/css";
+import { style } from "@vanilla-extract/css";
 import { recipe } from "@vanilla-extract/recipes";
 
 export const bannerContainerStyles = style({
