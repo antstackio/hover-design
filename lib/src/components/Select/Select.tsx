c0bb9cdafe7481d46cdba5dc57b41560717f2e89
saurabhsutar192 <saurabhsutar192@gmail.com>
2022-12-21 19:03:56 +0530

    bug: sonar bug fix

--- lib/src/components/Select/Select.tsx
+++ lib/src/components/Select/Select.tsx
@@ -213,7 +213,7 @@ const SelectComponent: ForwardRefRenderFunction<
       };
     })();
     if (useDropdownPortal) {
-      ElementsWithScrolls().map((arr: HTMLElement) =>
+      ElementsWithScrolls().forEach((arr: HTMLElement) =>
         arr?.addEventListener("scroll", () => {
           closeOnScroll();
           applyDropDownPortalPosition();
@@ -222,7 +222,7 @@ const SelectComponent: ForwardRefRenderFunction<
 
       window.addEventListener("resize", applyDropDownPortalPosition);
       return () => {
-        ElementsWithScrolls().map((arr: HTMLElement) =>
+        ElementsWithScrolls().forEach((arr: HTMLElement) =>
           arr?.removeEventListener("scroll", () => {
             closeOnScroll();
             applyDropDownPortalPosition();
